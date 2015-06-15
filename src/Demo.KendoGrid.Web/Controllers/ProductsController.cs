180525ef36d04940d59c36c8705fbb9eec27a8be
Todd Meinershagen <todd@meinershagen.net>
2015-06-15 17:38:11 -0500

    Removed waste from the products controller.

--- src/Demo.KendoGrid.Web/Controllers/ProductsController.cs
+++ src/Demo.KendoGrid.Web/Controllers/ProductsController.cs
@@ -11,22 +11,10 @@ namespace Demo.KendoGrid.Web.Controllers
 	{
 		readonly ProductsContext _db = new ProductsContext();
 
-		private bool ProductExists(int key)
-		{
-			return _db.Products.Any(p => p.Id == key);
-		}
-
 		[EnableQuery]
 		public IQueryable<Product> Get()
 		{
 			return _db.Products;
-		}
-
-		[EnableQuery]
-		public SingleResult<Product> Get([FromODataUri] int key)
-		{
-			IQueryable<Product> result = _db.Products.Where(p => p.Id == key);
-			return SingleResult.Create(result);
 		}
 
 		protected override void Dispose(bool disposing)
